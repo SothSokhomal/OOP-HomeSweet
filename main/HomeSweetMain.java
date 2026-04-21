9f86b1febc55d438b9c5058669a05734c04f36cc
Rose <rose@Rose.local>
2026-04-21 11:22:18 +0700

    fixed things

--- main/HomeSweetMain.java
+++ main/HomeSweetMain.java
@@ -8,14 +8,30 @@ import model.Student;
 public class HomeSweetMain {
     public static void main(String[] args) {
         
-        Landlord l1 = new Landlord(001, "Elizabeth", "123-456-7890", "abc123@gmail.com", "Phnom Penh");
-        Admin a1 = new Admin(001, "John Doe", "johndoe@gmail.com", "password123");
-        Student s1 = new Student(001, "Jane Smith", "janesmith@gmail.com", "987-654-3210", "studentpassword", "Contract A", "1234567890123");
-        House h1 = new House(001, "123 Main St", "Elizabeth", true, 500.00);
+        Landlord l1 = new Landlord("Elizabeth", "123-456-7890", "abc123@gmail.com", "Phnom Penh");
+        Landlord l2 = new Landlord("Rose", "123-456-7890", "abc123@gmail.com", "Phnom Penh");
+
+
+        Admin a1 = new Admin("John Doe", "johndoe@gmail.com", "password123!"); 
+        Admin a2 = new Admin("Jane jane", "janedoe@gmail.com", "password456!"); 
+
+        Student s1 = new Student("Jane Smith", "janesmith@gmail.com", "987-654-3210", "studentpassword@", "Contract A", "1234567890123");
+        Student s2 = new Student("Smith", "janesmith@gmail.com", "987-654-3210", "studentpassword@", "Contract A", "1234567890123");
+    
+
+        House h1 = new House("123 Main St", "Elizabeth", true, 500.00);
+        House h2 = new House("456 Oak Ave", "Rose", false, 600.00);
 
         System.out.println(l1);
+        System.out.println(l2);
+
         System.out.println(a1);
+        System.out.println(a2);
+
         System.out.println(s1);
+        System.out.println(s2);
+        
         System.out.println(h1);
+        System.out.println(h2);
     }
 }
