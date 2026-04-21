7bacaed27eb2743e3090e35af60f192864c3bbc0
Rose <rose@Rose.local>
2026-04-21 10:01:29 +0700

    added main package

---
+++ main/HomeSweetMain.java
@@ -0,0 +1,21 @@
+package main;
+
+import model.Admin;
+import model.House;
+import model.Landlord;
+import model.Student;
+
+public class HomeSweetMain {
+    public static void main(String[] args) {
+        
+        Landlord l1 = new Landlord(001, "Elizabeth", "123-456-7890", "abc123@gmail.com", "Phnom Penh");
+        Admin a1 = new Admin(001, "John Doe", "johndoe@gmail.com", "password123");
+        Student s1 = new Student(001, "Jane Smith", "janesmith@gmail.com", "987-654-3210", "studentpassword", "Contract A", "1234567890123");
+        House h1 = new House(001, "123 Main St", "Elizabeth", true, 500.00);
+
+        System.out.println(l1);
+        System.out.println(a1);
+        System.out.println(s1);
+        System.out.println(h1);
+    }
+}
